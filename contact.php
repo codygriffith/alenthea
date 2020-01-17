4bbea6053d4701662a65a943c6323c37b89538cf
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-01-17 16:47:53 -0500

    Delete contact.php

--- contact.php
+++
@@ -1,7 +0,0 @@
-<form class="flex row space-between wrap" id="homepageContact">
-    <h1 id="contactHeader" class="centerText">We Look Forward To Hearing From You</h1>
-    <input id="contactName" type="text" name="name" placeholder="Your name,">
-    <input id="contactEmail" type="email" name="email" placeholder="email,">
-    <textarea placeholder="and what we can do for you."></textarea>
-    <input id="contactSubmit" class="button" type="submit">
-</form>
