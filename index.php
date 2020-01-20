61678bc0e34b9827aabf05b5660a1c3bd817d948
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-01-20 03:06:35 -0500

    c

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 <link href="sections/footer.css" rel="stylesheet" type="text/css">
 <!-- <meta http-equiv="refresh" content="1"> -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<base href="http://www.alenthea.com/index.html">
+
 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
 </head>
