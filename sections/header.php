4d0649239ec5fd73e7dff82a228cc733ea71f1ee
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-01-24 14:12:54 -0500

    0

--- sections/header.php
+++ sections/header.php
@@ -9,17 +9,23 @@
 		<a id="headerContact" href="/pages/contactUs.php">Contact Us</a>
 </header>
 
-<mobileHeader class="flex column center dark white-text">
-	<div id="toggleBar" class="row flex center space-between full">
+<mobileHeader class="flex column dark white-text">
+	<div onclick="myFunction()" id="toggleBar" class="row flex center space-between full">
 	<a href="/index.php" id="headerLogo">
-			<img src="../alenthea/images/headerLogo.png">
+			<img src="/images/headerLogo.png">
 		</a>
 		<div id="toggle">=</div>
 	</div>
-		
-		<a href="/pages/webDesign.php">Web Design</a>
-		<a href="/pages/branding.php">Logos & Branding</a>
+	<div id="mobileHeaderLinks">
+		<a class="mobileHeaderLinks" href="/pages/webDesign.php">Web Design</a>
+		<a class="mobileHeaderLinks" href="/pages/branding.php">Logos & Branding</a>
 		<!-- <a href="/pages/ecommerce.php">E-Commerce</a> -->
-		<a href="/pages/onlineMarketing.php">Online Marketing</a>
-		<a id="headerContact" href="/pages/contactUs.php">Contact Us</a>
+		<a class="mobileHeaderLinks" href="/pages/onlineMarketing.php">Online Marketing</a>
+		<a class="mobileHeaderLinks" id="headerContact" href="/pages/contactUs.php">Contact Us</a>
+</div>
 </mobileHeader>
+<script>
+function myFunction() {
+  document.getElementById("mobileHeaderLinks").classList.toggle("toggle");
+}
+</script>
