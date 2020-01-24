5536839aecdd822854e7fd8d901f8383749dab85
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-01-23 21:17:23 -0500

    0

--- sections/header.php
+++ sections/header.php
@@ -6,5 +6,20 @@
 		<a href="/pages/branding.php">Logos & Branding</a>
 		<!-- <a href="/pages/ecommerce.php">E-Commerce</a> -->
 		<a href="/pages/onlineMarketing.php">Online Marketing</a>
-		<a href="/pages/contactUs.php">Contact Us</a>
+		<a id="headerContact" href="/pages/contactUs.php">Contact Us</a>
 </header>
+
+<mobileHeader class="flex column center dark white-text">
+	<div id="toggleBar" class="row flex center space-between full">
+	<a href="/index.php" id="headerLogo">
+			<img src="../alenthea/images/headerLogo.png">
+		</a>
+		<div id="toggle">bar</div>
+	</div>
+		
+		<a href="/pages/webDesign.php">Web Design</a>
+		<a href="/pages/branding.php">Logos & Branding</a>
+		<!-- <a href="/pages/ecommerce.php">E-Commerce</a> -->
+		<a href="/pages/onlineMarketing.php">Online Marketing</a>
+		<a id="headerContact" href="/pages/contactUs.php">Contact Us</a>
+</mobileHeader>
