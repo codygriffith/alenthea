50853279c72bdce3f40efd84da5c8ded1410eb92
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-01-30 22:24:26 -0500

    0

--- sections/footer.php
+++ sections/footer.php
@@ -1,14 +1,29 @@
 <style>
 <?php include 'style/footer.css'; ?>
 </style>
-<footer class="flex row center dark white-text thin-text">
-    <div id="footerPhone" class="flex footerContact column center">
-        <h1>Give Us A Call</h1>
-        <h2>(706)888-0976</h2>
+<footer>
+    <div class="desktop flex row center dark white-text thin-text">
+        <div id="footerPhone" class="flex footerContact column center">
+            <h1>Give Us A Call</h1>
+            <h2>(706)888-0976</h2>
+        </div>
+        <img id="footerLogo" src="/images/footerLogo.svg">
+        <div id="footerEmail" class="flex footerContact column center">
+            <h1>Send Us An Email</h1>
+            <h2>Hello@alenthea.com</h2>
+        </div>
     </div>
-    <img id="footerLogo" src="/images/footerLogo.svg">
-    <div id="footerEmail" class="flex footerContact column center">
-        <h1>Send Us An Email</h1>
-        <h2>Hello@alenthea.com</h2>
+    <div class="mobile flex row center dark white-text thin-text">
+        <img id="footerLogo" src="/images/headerLogo.png">
+        <div>
+            <div id="footerPhone" class="flex footerContact column center">
+                <h1>CALL</h1>
+                <h2>(706)888-0976</h2>
+            </div>
+            <div id="footerEmail" class="flex footerContact column center">
+               <h1>MAIL</h1>
+               <h2>Hello@alenthea.com</h2>
+            </div>
+        </div>
     </div>
 </footer>
--- sections/header.php
+++ sections/header.php
@@ -2,7 +2,8 @@
 <?php include 'style/header.css'; ?>
 </style>
 
-<header class="flex row center dark white-text">
+<header>
+	<div class="desktop flex row center dark white-text">
 		<a href="/index.php" id="headerLogo">
 			<img src="/images/alentheaFullLogo_light.svg">
 		</a>
@@ -11,23 +12,24 @@
 		<!-- <a href="/pages/ecommerce.php">E-Commerce</a> -->
 		<a href="/pages/onlineMarketing.php">Online Marketing</a>
 		<a id="headerContact" href="/pages/contactUs.php">Contact Us</a>
-</header>
-
-<mobileHeader class="flex column dark white-text">
-	<div onclick="myFunction()" id="toggleBar" class="row flex center space-between full">
-	<a href="/index.php" id="headerLogo">
-			<img src="/images/alentheaFullLogo_light.svg">
-		</a>
-		<div id="toggle">=</div>
 	</div>
-	<div id="mobileHeaderLinks">
-		<a class="mobileHeaderLinks" href="/pages/webDesign.php">Web Design</a>
-		<a class="mobileHeaderLinks" href="/pages/branding.php">Logos & Branding</a>
-		<!-- <a href="/pages/ecommerce.php">E-Commerce</a> -->
-		<a class="mobileHeaderLinks" href="/pages/onlineMarketing.php">Online Marketing</a>
-		<a class="mobileHeaderLinks" id="headerContact" href="/pages/contactUs.php">Contact Us</a>
-</div>
-</mobileHeader>
+
+	<div class="mobile flex column dark white-text">
+		<div onclick="myFunction()" id="toggleBar" class="row flex center space-between full">
+			<a href="/index.php" id="headerLogo">
+				<img src="/images/alentheaFullLogo_light.svg">
+			</a>
+			<div id="toggle">=</div>
+		</div>
+		<div id="mobileHeaderLinks">
+			<a class="mobileHeaderLinks" href="/pages/webDesign.php">Web Design</a>
+			<a class="mobileHeaderLinks" href="/pages/branding.php">Logos & Branding</a>
+			<!-- <a href="/pages/ecommerce.php">E-Commerce</a> -->
+			<a class="mobileHeaderLinks" href="/pages/onlineMarketing.php">Online Marketing</a>
+			<a class="mobileHeaderLinks" id="headerContact" href="/pages/contactUs.php">Contact Us</a>
+		</div>
+	</div>
+</header>
 <script>
 function myFunction() {
   document.getElementById("mobileHeaderLinks").classList.toggle("toggle");
