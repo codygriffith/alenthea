239a8b87951d9e20757d86291e7b8a53f162ea92
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-01-30 19:46:50 -0500

    Update contactUs.php

--- pages/contactUs.php
+++ pages/contactUs.php
@@ -5,6 +5,8 @@
 	<title>Contact Us</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="../style.css" rel="stylesheet" type="text/css">
+	<link href="style/page.css" rel="stylesheet" type="text/css">
+
 	<link href="style/contactUs.css" rel="stylesheet" type="text/css">
 </head>
 
