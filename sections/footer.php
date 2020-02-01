ea2bff61b9ab06ff5fb553f052e3d3433463d6b3
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-02-01 04:01:05 -0500

    Update footer.php

--- sections/footer.php
+++ sections/footer.php
@@ -7,14 +7,14 @@
             <h1>Give Us A Call</h1>
             <h2>(706)888-0976</h2>
         </div>
-        <img id="footerLogo" src="/images/footerLogo.svg">
+        <img id="footerLogo" src="/images/footerLogo.svg" alt="Alenthea Logo">
         <div id="footerEmail" class="flex footerContact column center">
             <h1>Send Us An Email</h1>
             <h2>Hello@alenthea.com</h2>
         </div>
     </div>
     <div class="mobile flex row center dark white-text thin-text">
-        <img id="footerLogo" src="/images/headerLogo.png">
+        <img id="footerLogo" src="/images/headerLogo.png" alt="Alenthea Logo">
         <div>
             <div id="footerPhone" class="flex footerContact column center">
                 <h1>CALL</h1>
