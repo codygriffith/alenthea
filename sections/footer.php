61c3a8b9e6d22032aedcaa4588469cc6528a4a31
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-02-01 20:08:33 -0500

    0

--- sections/footer.php
+++ sections/footer.php
@@ -12,7 +12,7 @@
             <h3>Send Us An Email</h3>
             <h2>Hello@alenthea.com</h2>
         </div>
-        <a href="https://twitter.com/alenthea2?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-screen-name="false" data-show-count="false">Follow @alenthea2</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        
     </div>
     <div class="mobile flex row center dark white-text thin-text">
         <img class="footerLogo" src="/images/headerLogo.png" alt="Alenthea Logo">
