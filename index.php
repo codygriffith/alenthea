e934570cf0eebf4475cf5be9e3d037556ce0010d
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-02-01 19:49:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -135,8 +135,7 @@
 	</main>
 	<?php include 'sections/contact.php';?>
 	<?php include 'sections/footer.php';?>
-	<!-- Go to www.addthis.com/dashboard to customize your tools -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5e3558f28660269b"></script>
+	<!-- Go to www.addthis.com/dashboard to customize your tools --> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5e3558f28660269b"></script>
 
 </body>
 
