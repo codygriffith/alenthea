5d28f7c6a3247e32e539f2c622131dd4745d93df
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-02-02 01:31:22 -0500

    0

--- pages/webDesign.php
+++ pages/webDesign.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
+<link rel="dns-prefetch" href="//google-analytics.com">
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-157506747-1"></script>
 <script>
