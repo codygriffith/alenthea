59dce0624022ef264365d0fbed7eda0562aa1ebd
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-02-02 02:53:30 -0500

    0

--- pages/branding.php
+++ pages/branding.php
@@ -63,7 +63,7 @@
 	</section>
     
 	<section>
-		<h1 id="servicesHeader" class="centerText">Our Services</h1>
+		<h1 class="centerText servicesHeader">Our Services</h1>
 		<ul class="flex row center wrap" id="servicesList">
 				<li>Logo Design</li>
             	<li>Business Cards</li>
@@ -77,7 +77,7 @@
 	</section>
 
 	<section id="propBoxes" class="flex column center">
-		<h1 id="servicesHeader" class="centerText">Our Services</h1>
+		<h1 class="centerText servicesHeader">Our Services</h1>
 		<div class="flex full center">
 			<p>Drive customer loyalty and make yourself recognizable to a broader audience.</p>
 			<p>The value of your brand can sometimes feel invisible, but when prices rise with demand the return becomes apparent.</p>
