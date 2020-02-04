cd91bb991eafa1751dbcf10cb2ba90eec3024cca
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-02-04 03:01:37 -0500

    Update sendForm.php

--- pages/sendForm.php
+++ pages/sendForm.php
@@ -1,3 +1,39 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+<link rel="canonical" href="https://alenthea.com/pages/contact" />
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157506747-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-157506747-1');
+</script>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<meta charset="utf-8">
+	<title>Contact Us</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="../style.css" rel="stylesheet" type="text/css">
+	<link href="style/page.css" rel="stylesheet" type="text/css">
+
+	<link href="style/contactUs.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<?php include "../sections/header.php";?>
+	<div class="flex column bigOlText">
+        <h1>Lets Do Something Great</h1> 
+	</div>
+	<?php include "../sections/contact.php";?>
+	<?php include '../sections/footer.php';?>
+</body>
+
 <?php   
     $name = $_POST['name'];
     $visitor_email = $_POST['email'];
@@ -22,22 +58,5 @@
         echo "Error: Message not accepted";
     }
 ?>
-<style>
-<?php include 'style/contact.css'; ?>
-</style>
-<form method="post" action="/pages/sendForm.php" class="flex row space-between wrap" id="homepageContact">
-    <h3 class="centerText contactHeader">We Look Forward To Hearing From You</h3>
-    <h1 id="mobileContactHeader" class="centerText contactHeader">Lets Get Started</h1>
-
-    <label for="contactName">Your Name:</label>
-    <input id="contactName" type="text" name="name" placeholder="Your name,">
-
-    <label for="contactEmail">Your Email:</label>
-    <input id="contactEmail" type="email" name="email" placeholder="email,">
-
-    <label for="moreInfo">How may we help you?</label>
-    <textarea name="message" id="moreInfo" placeholder="and what we can do for you."></textarea>
-
-    <label for="promo">Submit</label>
-    <input id="contactSubmit" class="button" type="submit">
-</form>
+	
+</html>
