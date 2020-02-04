6ff15ce178a1d2163a4c1c61fb650e7380c7dbab
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-02-04 02:00:05 -0500

    Update header.php

--- sections/header.php
+++ sections/header.php
@@ -5,7 +5,7 @@
 <header>
 	
 	<div class="desktop flex row center dark white-text">
-		<a href="/index.php" id="headerLogo">
+		<a href="/" id="headerLogo">
 			<img src="/images/alentheaFullLogo_light.svg" alt="Alenthea Logo">
 		</a>
 		<a href="/pages/web-design">Web Design</a>
@@ -17,7 +17,7 @@
 
 	<div class="mobile flex column dark white-text">
 		<div onclick="myFunction()" id="toggleBar" class="row flex center space-between full">
-			<a href="/index.php" id="headerLogo">
+			<a href="/" id="headerLogo">
 				<img src="/images/alentheaFullLogo_light.svg" alt="Alenthea Logo">
 			</a>
 			<div id="toggle">=</div>
