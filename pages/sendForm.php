a7a36c052419adacc4b2c5bdea006fadf483c822
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-02-04 03:00:08 -0500

    0

--- pages/sendForm.php
+++ pages/sendForm.php
@@ -22,3 +22,22 @@
         echo "Error: Message not accepted";
     }
 ?>
+<style>
+<?php include 'style/contact.css'; ?>
+</style>
+<form method="post" action="/pages/sendForm.php" class="flex row space-between wrap" id="homepageContact">
+    <h3 class="centerText contactHeader">We Look Forward To Hearing From You</h3>
+    <h1 id="mobileContactHeader" class="centerText contactHeader">Lets Get Started</h1>
+
+    <label for="contactName">Your Name:</label>
+    <input id="contactName" type="text" name="name" placeholder="Your name,">
+
+    <label for="contactEmail">Your Email:</label>
+    <input id="contactEmail" type="email" name="email" placeholder="email,">
+
+    <label for="moreInfo">How may we help you?</label>
+    <textarea name="message" id="moreInfo" placeholder="and what we can do for you."></textarea>
+
+    <label for="promo">Submit</label>
+    <input id="contactSubmit" class="button" type="submit">
+</form>
