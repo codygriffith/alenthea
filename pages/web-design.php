280adab95fca114eb94788e85c74ab5f6cdb726c
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-02-19 18:15:14 -0500

    Update web-design.php

--- pages/web-design.php
+++ pages/web-design.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html lang="en">
 <head>
-
 <link rel="canonical" href="https://alenthea.com/pages/columbus-ga-web-design" />
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-157506747-1"></script>
@@ -21,16 +20,16 @@
 	<link href="style/webDesign.css" rel="stylesheet" type="text/css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="utf-8">
-	<title>Columbus, GA Web Design - Alenthea</title>
+	<title>Columbus GA Web Design</title>
 	<meta name="description" content="Web Design & Development in Columbus, Georgia. Out class and out-rank your competetion with a strong hand-crafted, online experience." />
 	<script type="application/ld+json">
 		{
 		  "@context": "https://schema.org",
 		  "@type": "Organization", 
-		  "url": "https://alenthea.com/",
+		  "url": "https://alenthea.com",
 		  "logo": "https://alenthea.com/images/alentheaFullDark.png",
 		  "areaServed": "Columbus, Georgia, Alabama, Atlanta",
-		  "brand": "Alenthea, Alenthea Design Co"
+		  "brand": "Alenthea Design Co"
 		  
 		}</script>
         <script type="application/ld+json">
@@ -45,7 +44,7 @@
   	    	},{
         	  "@type": "ListItem",
           	  "position": 2,
-          	  "name": "Alenthea Web Design",
+          	  "name": "Columbus Ga Web Design",
               "item": "https://alenthea.com/pages/columbus-ga-web-design"
             }]
 		}
@@ -53,50 +52,48 @@
 		</script>
 </head>
 <body>
-	<?php include "../sections/header.php";?>
-	<h1></h1>
-	<div class="flex column bigOlText">
-            <img src="/images/webDesign2.svg" alt="Web Design"> 
-        </div>
-        <h3 class="littleOlText">Out-class and out-rank your competition with a strong hand-crafted, online experience.</h3>
+    <?php include "../sections/header.php";?>
+    
+	<h1 class="flex column bigOlText">
+            <span>Columbus Ga</span>
+            Web Design
+        </h1>
+    <h3 class="littleOlText">Out-class and out-rank your competition with a strong hand-crafted, online experience.</h3>
 	
-	
-	<div class="full flex center">
-		<h4 class="flex column center sectionHeader">Loaded with features:</h4>
-	</div>
-
-	<div class="flex row wrap center" id="serviceTiles">
-		<div>
-			<h1>Mobile Ready</h1>
-			<p>All of our websites are built responsively and with all possible screensizes in mind.</p>
-			<!-- <a class="button">Yeet me</a> -->
-
-		</div>
-		<div>
-			<h1>Dynamic Content</h1>
-			<p>Dynamic web sites are faster, more secure, and deliver fresh content.</p>
-			<!-- <a class="button">Yeet me</a> -->
-
-		</div>
-		<div>
-			<h1>Domain Email</h1>
-			<p>Impress your audience with your own "@yourdomain" custom email address.</p>
-			<!-- <a class="button">Yeet me</a> -->
-
-		</div>
-		<div>
-			<h1>Intelligent S.E.O.</h1>
-			<p>Place your content in front of the right people when they need you most.</p>
-			<!-- <a class="button">Yeet me</a> -->
-		</div>
-	</div>
-
-	<section class="options">
-		<h1>Custom Websites</h1>
-		<p>Develop a custom, ground-up online experience unique to your brand only. Our custom website designs are the most 
-		efficient and optimized solutions for a true professional user experience.</p>
+	<section class="full flex center">
+		<h1 class="flex column center sectionHeader">Loaded with features:</h4>
 	</section>
 
+	<section class="flex row wrap center" id="serviceTiles">
+		<section>
+			<h1>Mobile Ready</h1>
+			<p>All of our websites are built responsively and with all possible screensizes in mind.</p>
+		</section>
+		<section>
+			<h1>Dynamic Content</h1>
+			<p>Dynamic web sites are faster, more secure, and deliver fresh content.</p>
+		</section>
+		<section>
+			<h1>Domain Email</h1>
+			<p>Impress your audience with your own "@yourdomain" custom email address.</p>
+		</section>
+		<section>
+			<h1>Intelligent S.E.O.</h1>
+			<p>Place your content in front of the right people when they need you most.</p>
+		</section>
+	</section>
+
+	<!-- <section class="options">
+		<h1>Custom Websites</h1>
+		<p>We develop a custom, ground-up online experience unique to your brand only. Our custom website designs are the most 
+		efficient and optimized solutions for a true professional user experience.</p>
+        <h1>Wordpress Development</h1>
+		<p>We develop a custom, ground-up online experience unique to your brand only. Our custom website designs are the most 
+		efficient and optimized solutions for a true professional user experience.</p>
+        <h1>Custom Websites</h1>
+		<p>We develop a custom, ground-up online experience unique to your brand only. Our custom website designs are the most 
+		efficient and optimized solutions for a true professional user experience.</p>
+	</section> -->
 
 
 
@@ -104,20 +101,23 @@
 
 
 
-	<div class="flex column bigOlText secondBigText">
-		<h1>Latest Technologies;</h1>
-		<h1>Highest Standards.</h1>
-	</div>
-	<p class="littleOlText secondLittleText">Our websites work exceptionally well to deliver and intuitive and encapsulating experience for both your audience and yourself alike.</p>
+
+    <section>
+	    <div class="flex column bigOlText secondBigText">
+		    <h1>Latest Technologies;</h1>
+		    <h1>Highest Standards.</h1>
+	    </div>
+        <p class="littleOlText secondLittleText">Our websites work exceptionally well to deliver and intuitive and encapsulating experience for both your audience and yourself alike.</p>
+    </section>
 	<section class="flex center row" id="hosting">
-		<img src="/images/server.jpg" alt="Server Illustration">
+		<img src="/images/server.jpg" alt="Hosting Illustration">
 		<div class="flex center column">
 			<h1>HOSTING</h1>
 			<h2>Low to No Cost</h2>
 			<a class="button">Learn More</a>
 		</div>
 	</section>
-	<section id="ecommerce" class="flex column bigOlText">
+	<section id="ecommerce" class="flex column bigOlText secondBigText">
 		<div>
 			<h1>E-Commerce</h1>
 			<p>Take your business online and market your products across the most popular platforms.<p>
