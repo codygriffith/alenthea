bd2cea28ab8bed4aae86c9dd87745260a79b94ea
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-02-20 00:31:56 -0500

    Update blog.php

--- pages/blog.php
+++ pages/blog.php
@@ -1,8 +1,8 @@
 <!doctype html>
 <html lang="en">
 <head>
-
-<link rel="canonical" href="/" />
+<link rel="canonical" href="https://alenthea.com/pages/columbus-ga-web-design" />
+	<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-157506747-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -11,17 +11,27 @@
 
   gtag('config', 'UA-157506747-1');
 </script>
-
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link href="../style.css" rel="stylesheet" type="text/css">
+	<link href="style/page.css" rel="stylesheet" type="text/css">
+	<link href="style/webDesign.css" rel="stylesheet" type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<title>Columbus GA Web Design</title>
+	<meta name="description" content="Web Design & Development in Columbus, Georgia. Out class and out-rank your competetion with a strong hand-crafted, online experience." />
 	<script type="application/ld+json">
 		{
 		  "@context": "https://schema.org",
-		  "@type": "Organization",
-		  "url": "http://www.alenthea.com",
-		  "logo": "http://www.alenthea.com/images/alentheaFullDark.png",
+		  "@type": "Organization", 
+		  "url": "https://alenthea.com",
+		  "logo": "https://alenthea.com/images/alentheaFullDark.png",
 		  "areaServed": "Columbus, Georgia, Alabama, Atlanta",
-		  "brand": "Alenthea, Alenthea Design Co"
-		}"
-		</script>
+		  "brand": "Alenthea Design Co"
+		  
+		}</script>
         <script type="application/ld+json">
         {
 		  "@context": "https://schema.org",
@@ -29,130 +39,93 @@
   			"itemListElement": [{
          	  "@type": "ListItem",
           	  "position": 1,
-         	  "name": "Alenthea Blog",
-         	  "item": "http://alenthea.com/pages/blog"
-  	    	}]
+         	  "name": "Alenthea Design Co",
+         	  "item": "https://alenthea.com"
+  	    	},{
+        	  "@type": "ListItem",
+          	  "position": 2,
+          	  "name": "Columbus Ga Web Design",
+              "item": "https://alenthea.com/pages/columbus-ga-web-design"
+            }]
 		}
         
 		</script>
-		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
-<meta charset="utf-8">
-<title>Web Design, SEO, Marketing Blog</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="/style.css" rel="stylesheet" type="text/css">
-<link href="/index.css" rel="stylesheet" type="text/css">
-<meta name="description" content="We are a full-service design agency operating in Columbus, GA. We specialize in Web Design, Digital Marketing, logos, and Branding. Check out our our low to no cost hosting options." />
-<meta name="keywords" content="Web, Design, Logos, Branding, Online, Marketing, Development" />
-<!-- <meta http-equiv="refresh" content="1"> -->
-<!-- <base href="http://www.alenthea.com/index.html"> -->
-
-<!-- <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"> -->
-
 </head>
-
 <body>
-	<?php include '/sections/header.php';?>
-	<main>
-		<section id="heroImage" class="flex center">
-			<img src="/images/alentheaFullLogo.svg" alt="Alenthea Logo">
+    <?php include "../sections/header.php";?>
+    
+	<h1 class="flex column bigOlText">
+            <span>Columbus Ga</span>
+            Web Design
+        </h1>
+    <h3 class="littleOlText">Out-class and out-rank your competition with a strong hand-crafted, online experience.</h3>
+	
+	<section class="full flex center">
+		<h1 class="flex column center sectionHeader">Loaded with features:</h4>
+	</section>
+
+	<section class="flex row wrap center" id="serviceTiles">
+		<section>
+			<h1>Mobile Ready</h1>
+			<p>All of our websites are built responsively and with all possible screensizes in mind.</p>
 		</section>
-		<section class="flex row center full wrap" id="services">
-			<section id="webDesign" class="flex column center serviceTile light">
-				<div class="flex medium center serviceIconContainer">
-					<picture>
-						<source media="(max-width: 800px)" srcset="/images/webDev-darkest.svg">
-						<source media="(min-width: 800px)" srcset="/images/webDev-light.svg">
-						<img src="/images/webDev-dark.svg" alt="Web Design">
-					</picture>
-				</div>
-				<div class="serviceTileContent flex column center">
-					<h1>Web Design</h1>
-					<p>Mobile friendly, responsive, and beautiful across all devices.</p>	
-					<a href="/pages/columbus-ga-web-design" class="button">View More</a>
-				</div>
-			</section>
-			<section id="ecommerce" class="flex column center serviceTile light">
-				<div class="flex medium center serviceIconContainer">
-					<picture>
-						<source media="(max-width: 800px)" srcset="/images/ecommerce-darkest.svg">
-						<source media="(min-width: 800px)" srcset="/images/ecommerce-light.svg">
-						<img src="/images/ecommerce-light.svg" alt="E-Commerce">
-					</picture>
-				</div>
-				<div class="serviceTileContent flex column center">
-					<h1>E-Commerce</h1>
-					<p>Grow your customer base and reach new potential.</p>	
-					<a href="/pages/ecommerce" class="button">View More</a>
-				</div>
-			</section>
-			<section id="onlineMarketing" class="flex column center serviceTile light">
-				<div class="flex medium center serviceIconContainer">
-					<picture>
-						<source media="(max-width: 800px)" srcset="/images/onlineMarketing-darkest.svg">
-						<source media="(min-width: 800px)" srcset="/images/onlineMarketing-light.svg">
-						<img src="/images/onlineMarketing-darkest.svg" alt="Online Marketing">
-					</picture>
-				</div>
-				<div class="serviceTileContent flex column center">
-					<h1>Online Marketing</h1>
-					<p>Spread awareness for your products or brand.</p>	
-					<a href="/pages/marketing" class="button">View More</a>
-				</div>
-			</section>
-			<section id="logosBranding" class="flex column center serviceTile light">
-				<div class="flex medium center serviceIconContainer">
-					<picture>
-						<source media="(max-width: 800px)" srcset="/images/logoBranding-darkest.svg">
-						<source media="(min-width: 800px)" srcset="/images/logoBranding-light.svg">
-						<img src="/images/logoBranding-light.svg" alt="Logos & Branding">
-					</picture>
-				</div>
-				<div class="serviceTileContent flex column center">
-					<h1>Logos & Branding</h1>
-					<p>Give your brand a new, fresh, and timeless appearance.</p>	
-					<a href="/pages/branding" class="button">View More</a>
-				</div>
-			</section>
+		<section>
+			<h1>Dynamic Content</h1>
+			<p>Dynamic web sites are faster, more secure, and deliver fresh content.</p>
 		</section>
-
-		<section id="openingPitch">
-			<p id="homepage-pitch" class="font-two">
-				 
-				Our 
-				<span class="text-highlight">Hand-Crafted</span> 
-				Solutions  are uniquely developed for each individual client for a product that stands out from the 
-				rest and delivers 
-				<span class="text-highlight">Complete Satisfaction</span> 
-				for both yourself and users alike.
-			</p>
+		<section>
+			<h1>Domain Email</h1>
+			<p>Impress your audience with your own "@yourdomain" custom email address.</p>
 		</section>
-	<div class="flex center row light" id="missionBanner">
-		<img src="/images/alentheaFullLogoDark.svg" alt="Alenthea Logo">
-		<h3>Our Mission Is To Help Our Clients Realize Their Dreams,<br> No Matter How Big Or Small.</h3>
-	</div>
-
-
-
-		<section id="goals" class="flex center column">
-			<h2>Achieve Your Goals</h2>
-			<div class="flex row center">
-				<p>Generate more leads and market your services locally and nationally. Promote trust in your brand and show your expertise.</p>
-				<p>Rank higher in organic search results and make yourself discoverable on the web. Increase your presence in local search.</p>
-				<p>Refresh your brand identity and maintain a modern appearance in today’s ever evolving market.</p>
-			</div>
+		<section>
+			<h1>Intelligent S.E.O.</h1>
+			<p>Place your content in front of the right people when they need you most.</p>
 		</section>
+	</section>
+
+	<!-- <section class="options">
+		<h1>Custom Websites</h1>
+		<p>We develop a custom, ground-up online experience unique to your brand only. Our custom website designs are the most 
+		efficient and optimized solutions for a true professional user experience.</p>
+        <h1>Wordpress Development</h1>
+		<p>We develop a custom, ground-up online experience unique to your brand only. Our custom website designs are the most 
+		efficient and optimized solutions for a true professional user experience.</p>
+        <h1>Custom Websites</h1>
+		<p>We develop a custom, ground-up online experience unique to your brand only. Our custom website designs are the most 
+		efficient and optimized solutions for a true professional user experience.</p>
+	</section> -->
 
 
 
-	</main>
-	<?php include 'sections/contact.php';?>
-	<a href="https://twitter.com/alentheac?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-screen-name="false" data-show-count="false">Follow @alenthea2</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>	
-	<?php include 'sections/footer.php';?>
 
+
+
+
+
+    <section>
+	    <div class="flex column bigOlText secondBigText">
+		    <h1>Latest Technologies;</h1>
+		    <h1>Highest Standards.</h1>
+	    </div>
+        <p class="littleOlText secondLittleText">Our websites work exceptionally well to deliver and intuitive and encapsulating experience for both your audience and yourself alike.</p>
+    </section>
+	<section class="flex center row" id="hosting">
+		<img src="/images/server.jpg" alt="Hosting Illustration">
+		<div class="flex center column">
+			<h1>HOSTING</h1>
+			<h2>Low to No Cost</h2>
+			<a class="button">Learn More</a>
+		</div>
+	</section>
+	<section id="ecommerce" class="flex column bigOlText secondBigText">
+		<div>
+			<h1>E-Commerce</h1>
+			<p>Take your business online and market your products across the most popular platforms.<p>
+		</div>
+	</section>
+	<?php include "../sections/contact.php";?>
+	<br><br>
+	<?php include "../sections/footer.php";?>
 </body>
-
 	
 </html>
