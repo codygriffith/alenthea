7b21fa11d2b020b69de5312f013b045b200dbbe5
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-02-20 23:02:15 -0500

    0

--- blog/post/semantic-tags-search-engine-optimization.php
+++ blog/post/semantic-tags-search-engine-optimization.php
@@ -44,7 +44,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="/style.css" rel="stylesheet" type="text/css">
 <link href="/index.css" rel="stylesheet" type="text/css">
-<link href="/pages/style/blog.css" rel="stylesheet" type="text/css">
+<link href="/pages/style/post.css" rel="stylesheet" type="text/css">
+
 <meta name="description" content="We are a full-service design agency operating in Columbus, GA. We specialize in Web Design, Digital Marketing, logos, and Branding. Check out our our low to no cost hosting options." />
 <meta name="keywords" content="Web, Design, Logos, Branding, Online, Marketing, Development" />
 <!-- <meta http-equiv="refresh" content="1"> -->
@@ -55,23 +56,41 @@
 </head>
 
 <body>
-	<?php include '../sections/header.php';?>
+	<?php include '../../sections/header.php';?>
 	<main>
 		<article>
             <h1>Semantic Tags and Search Engine Optimization</h1>
             <p>Semantic markup is another way of helping Google, other search engines and even screen readers understand your websites structure and context of the available 
-                content. 
-                </p>
-            <a href="/blog/post/semantic-tags-search-engine-optimization">Read Post</a>
-
+                content.</p>
+                <br>
+            <p> Most of us are already familiar with some semantic tags, but it is crucial that we become more comfortable with what they are and how to use them. 
+                As time evolves, search engines will move to prioritize sites with semantically correct markup.</p>
+            <section>
+                <h1>Semantic tags are HTML5 elements</h1>
+                <p>HTML5 is the latest version of HTML - the markup language powering the web. With its release came a handful of new elements. These elements are used to better define the different sections of a webpage to give better meaning to their content.</p>
+            </section>
+            <section>
+                <h1>Different semantic tags to use:</h1>
+                <ul>
+                    <li>&lt;article&gt; elements define context that generally can live independent of the rest of the site. These commonly are used with Forum posts, News Articles, and Blog posts.</li>
+                    <li>The &lt;aside&gt; tag is used to define related content that is placed beside content, like a call to action inside of a sidebar.</li>
+                    <li>The &lt;aside&gt; tag is used to define related content that is placed beside content, like a call to action inside of a sidebar.</li>
+                    <li>A &lt;figure&gt; and &lt;figcaption&gt; tag are used with an image and caption pair to add additional information to an image.</li>
+                    <li>The &lt;footer&gt; tag defines the footing for a section of a webpage. The footer tag should contain information about its parent container like contact information, important links, and document author.</li>
+                    <li>The &lt;header&gt; tag contains the introductory content of a section. </li>
+                    <li>The &lt;main&gt; tag defines the main content that is unique to each webpage. You can have multiple <main> tags on a webpage.</li>
+                    <li>The &lt;nav&gt; tag defines a major set of navigation links. Typically this would be found in the header and footer.</li>
+                    <li>A &lt;section&gt; element defines individual sections on a webpage. These could be used on a home page to split portions of a page into sections. The introduction would be its own section, as well as the contact info, Q&A section, etc. </li>
+                </ul>
+            </section>
     </article>
 
 
 
 	</main>
-	<?php include '../sections/contact.php';?>
+	<?php include '../../sections/contact.php';?>
 	<a href="https://twitter.com/alentheac?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-screen-name="false" data-show-count="false">Follow @alenthea2</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>	
-	<?php include '../sections/footer.php';?>
+	<?php include '../../sections/footer.php';?>
 
 </body>
 
