5a9afe0023dada6e20058e76422154d892c00e63
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-02-25 08:50:54 -0500

    0

---
+++ services/index.php
@@ -0,0 +1,130 @@
+<!doctype html>
+<html lang="en">
+<head>
+<link rel="canonical" href="https://alenthea.com/services/columbus-ga-web-design" />
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157506747-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-157506747-1');
+</script>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link href="/style.css" rel="stylesheet" type="text/css">
+	<link href="/pages/style/page.css" rel="stylesheet" type="text/css">
+    <link href="/pages/style/webDesign.css" rel="stylesheet" type="text/css">
+    <link href="/services/style/style.css" rel="stylesheet" type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<title> Our Services | Alenthea Design Co.</title>
+	<meta name="description" content="Web Design & Development in Columbus, Georgia. Out class and out-rank your competetion with a strong hand-crafted, online experience." />
+	<script type="application/ld+json">
+		{
+		  "@context": "https://schema.org",
+		  "@type": "Organization", 
+		  "url": "https://alenthea.com",
+		  "logo": "https://alenthea.com/images/alentheaFullDark.png",
+		  "areaServed": "Columbus, Georgia, Alabama, Atlanta",
+		  "brand": "Alenthea Design Co"
+		  
+		}</script>
+        <script type="application/ld+json">
+        {
+		  "@context": "https://schema.org",
+		  "@type": "BreadcrumbList",
+  			"itemListElement": [{
+         	  "@type": "ListItem",
+          	  "position": 1,
+         	  "name": "Alenthea Design Co",
+         	  "item": "https://alenthea.com"
+  	    	},{
+        	  "@type": "ListItem",
+          	  "position": 2,
+          	  "name": "Design Services",
+              "item": "https://alenthea.com/services"
+            }]
+		}
+        
+		</script>
+</head>
+<body>
+    <?php include "../sections/header.php";?>
+    
+	<h1 class="flex column bigOlText">Our Services</h1>
+    <h2 class="littleOlText">We use cutting-edge techniques and our high quality standards to develop custom solutionsa and achieve the results you need.</h2>
+	
+	<section class="full flex center">
+		<h1 class="flex column center sectionHeader">Loaded with features:</h4>
+	</section>
+
+	<section class="flex row wrap center" id="serviceTiles">
+		<section>
+			<h1>Mobile Ready</h1>
+			<p>All of our websites are built responsive and with all possible screensizes in mind.</p>
+		</section>
+		<section>
+			<h1>Dynamic Content</h1>
+			<p>Dynamic web sites are faster, more secure, and deliver fresh content.</p>
+		</section>
+		<section>
+			<h1>Domain Email</h1>
+			<p>Impress your clients with your own custom business email address.</p>
+		</section>
+		<section>
+			<h1>Intelligent S.E.O.</h1>
+			<p>Place your content in front of the right people when they need you most.</p>
+		</section>
+	</section>
+
+	<!-- <section class="options">
+		<h1>Custom Websites</h1>
+		<p>We develop a custom, ground-up online experience unique to your brand only. Our custom website designs are the most 
+		efficient and optimized solutions for a true professional user experience.</p>
+        <h1>Wordpress Development</h1>
+		<p>We develop a custom, ground-up online experience unique to your brand only. Our custom website designs are the most 
+		efficient and optimized solutions for a true professional user experience.</p>
+        <h1>Custom Websites</h1>
+		<p>We develop a custom, ground-up online experience unique to your brand only. Our custom website designs are the most 
+		efficient and optimized solutions for a true professional user experience.</p>
+	</section> -->
+
+
+
+
+
+
+
+
+	<br>
+    <section>
+	    <div class="flex column bigOlText secondBigText">
+		    <h1>Latest Technologies;</h1>
+		    <h1>Highest Standards.</h1>
+	    </div>
+        <p class="littleOlText secondLittleText">Our websites work exceptionally well to deliver and intuitive and encapsulating experience for both your audience and yourself alike.</p>
+    </section>
+	<section class="flex center row" id="hosting">
+		<img src="/images/server.jpg" alt="Hosting Illustration">
+		<div class="flex center column">
+			<h1>HOSTING</h1>
+			<h2>Low to No Cost</h2>
+			<!-- <a class="button">Learn More</a> -->
+		</div>
+	</section>
+	<section id="ecommerce" class="flex column bigOlText secondBigText">
+		<div>
+			<h1>E-Commerce</h1>
+			<p>Take your business online and market your products across the most popular platforms.<p>
+		</div>
+	</section>
+	<?php include "../sections/contact.php";?>
+	<br><br>
+	<?php include "../sections/footer.php";?>
+</body>
+	
+</html>
