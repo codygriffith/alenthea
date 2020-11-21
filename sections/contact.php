3ebf2c126e5d9797c22bef2f0d67814e88625b4b
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-11-21 01:59:01 -0500

    Update contact.php

--- sections/contact.php
+++ sections/contact.php
@@ -2,7 +2,7 @@
 <?php include 'style/contact.css'; ?>
 </style>
 <form method="post" action="/pages/sendForm.php" class="flex column space-between wrap" id="homepageContact">
-    <h3 class="centerText contactHeader">We Look Forward To Hearing From You</h3>
+    <h1 class="centerText contactHeader">We Look Forward To Hearing From You</h1>
     <h1 id="mobileContactHeader" class="centerText contactHeader">Lets Get Started</h1>
     <div>
     <label for="contactName">Your Name:</label>
