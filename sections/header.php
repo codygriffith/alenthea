742adca4e5daf4b2e09fd0ff91b0312514330b46
codygriffith <35708679+codygriffith@users.noreply.github.com>
2020-12-17 12:27:32 -0500

    0

--- sections/header.php
+++ sections/header.php
@@ -1,9 +1,52 @@
 <style>
 <?php include 'style/header.css'; ?>
 </style>
+
 <header>
-	
-<ul class="desktop flex row dark white-text space-evenly">
+	<div class="desktop flex row space-between">
+		<a href="/" id="companyLogo">
+			<img src="/images/alentheaFullLogo_light.svg" alt="Alenthea Logo">
+		</a>
+		<div class="desktop flex column" id="header-right">
+			<ul class="desktop flex row center white-text" id="social_links">
+				<li><a href="https://www.facebook.com/AlentheaDesignCo"><img src="/images/icons/social_media/facebook.svg" alt="ff"></a></li>
+				<li><a href="https://twitter.com/AlentheaDesign"><img src="/images/icons/social_media/twitter.svg" alt="ff"></a></li>
+				<li><a href="https://www.instagram.com/alentheadesign/"><img src="/images/icons/social_media/instagram.svg" alt="ff"></a></li>
+			</ul>
+			<nav>
+				<ul class="desktop flex row white-text space-evenly">
+					<div>
+						<li><a>Services</a></li>
+						<ul>
+							<li><a href="/services/columbus-ga-web-design">Website Design</a></li>
+							<li><a href="/services/digital-marketing-columbus-ga">Digital Marketing</a></li>
+							<li><a href="/services/branding">Logos & Branding</a></li>
+							<li><a href="/services/ecommerce">E-Commerce</a></li>
+							<!-- <li><a href="/services/ecommerce.php">S.E.O.</a></li>-->
+						</ul>
+					</div>
+					<li><a href="/pages/blog">Our Blog</a></li>
+					<li><a id="headerContact" href="/pages/contact">Contact Us</a></li>
+				</ul>
+			</nav>
+			
+</ul>
+		</div>
+	</div>
+</header>
+
+
+
+
+
+<header>
+	<!-- <div>
+		<ul class="desktop flex row center dark white-text" id="social_links">
+			<li><a href=""><img src="/images/icons/social_media/facebook.svg" alt="ff"></a></li>
+			<li><a href=""><img src="/images/icons/social_media/twitter.svg" alt="ff"></a></li>
+			<li><a href=""><img src="/images/icons/social_media/instagram.svg" alt="ff"></a></li>
+		</ul>	
+	<ul class="desktop flex row dark white-text space-evenly">
 	<li>
 		<a href="/" id="companyLogo">
 			<img src="/images/alentheaFullLogo_light.svg" alt="Alenthea Logo">
@@ -24,14 +67,8 @@
 			<li>
 				<a href="/services/ecommerce">E-Commerce</a>
 			</li>
-			<!-- <li>
-				<a href="/services/ecommerce.php">S.E.O.</a>
-			</li>			 -->
 		</ul>
 	</li>
-	<!-- <li>
-		<a href="/pages/branding">Our Work</a>
-	</li> -->
 	<li>
 		<a href="/pages/blog">Our Blog</a>
 	</li>
@@ -39,15 +76,7 @@
 		<a id="headerContact" href="/pages/contact">Contact Us</a>
 	</li>
 </ul>
-
-
-	<ul class="desktop flex row center dark white-text">
-		
-		
-		
-		<!--  -->
-</ul>
-
+</div> -->
 	<div class="mobile flex column dark white-text">
 		<div onclick="myFunction()" id="toggleBar" class="row flex space-between full">
 			<a href="/" id="mobileCompanyLogo">
@@ -61,12 +90,6 @@
 			<a class="mobileHeaderLinks" href="/services/branding">Logos & Branding</a>
 			<a class="mobileHeaderLinks" href="/services/digital-marketing-columbus-ga">Digital Marketing</a>
 			<a class="mobileHeaderLinks" href="/pages/contact">Contact Us</a>
-			<!-- <li></li>
-			<li></li>
-			<li></li>
-			<a href="/sections/ecommerce.php">E-Commerce</a>
-			<li></li>
-			<li></li> -->
 		</ul>
 </ul>
 </header>
---
+++ services/columbus-ga-seo.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html lang="en">
+<head>
+<link rel="canonical" href="https://alenthea.com/services/columbus-ga-web-design" />
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157506747-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-157506747-1');
+</script>
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link href="/style.css" rel="stylesheet" type="text/css">
+<link href="/services/style/style.css" rel="stylesheet" type="text/css">
+<link href="/pages/style/page.css" rel="stylesheet" type="text/css">
+<link href="/pages/style/webDesign.css" rel="stylesheet" type="text/css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<title>Web Design Columbus, GA | Alenthea Website Development</title>
+<meta name="description" content="Web Design & Development in Columbus, Georgia. 
+	Out-class and out-rank your competition with a custom and highly effective website design. 
+	We are the top choice in Web Design & Development in Columbus, Ga and the Southeastern US." />
+<script type="application/ld+json">
+	{
+	  "@context": "https://schema.org",
+	  "@type": "Organization", 
+	  "url": "https://alenthea.com",
+	  "logo": "https://alenthea.com/images/alentheaFullDark.png",
+	  "areaServed": "Columbus, GA",
+	  "brand": "Alenthea Design Co."
+	}
+</script>
+<script type="application/ld+json">
+    {
+	  "@context": "https://schema.org",
+	  "@type": "BreadcrumbList",
+  		"itemListElement": [{
+     	  "@type": "ListItem",
+      	  "position": 1,
+     	  "name": "Alenthea Design Co",
+     	  "item": "https://alenthea.com"
+  		},{
+    	  "@type": "ListItem",
+      	  "position": 2,
+      	  "name": "Columbus Ga Web Design",
+          "item": "https://alenthea.com/services/columbus-ga-web-design"
+        }]
+	}
+</script>
+</head>
+<body>
+    <?php include "../sections/header.php";?>
+	<h1 class="flex column bigOlText">
+		Search Engine Optimization
+    </h1>
+    <h2 class="littleOlText">Bring more organic traffic to your website and out-rank the competition with our professional SEO services for Columbus, Ga.</h2>
+	
+	
+
+	<section class="full flex center column" id="columbusGaWebDesign">
+		<h1 class="flex column center sectionHeader">Expert SEO In Columbus, Ga.</h1>
+		<div class="full flex center column">
+		<h2>Your local choice for professional SEO services.</h2>
+		<p class="font-two">If you are searching for the top choice for SEO, then you have found it with Alenthea. 
+		We employ the latest and greatest techniques that are guaranteed to not only rank your website fast, but 
+		to also make sure it stays there.
+		</p>
+	</div>
+	</section>
+
+	<!-- <section class="flex center row" id="pricing_section">
+			<section>
+				<h1>Get A Professional Website<br>
+					 @ A Sensible Budget</h1>
+					 <hr>
+				<div>
+					<H2>Get started today for as low as $500 for a Basic WordPress Website and spread your message across the internet.</H2>
+					<ul>
+						<li>Great for small businesses that require a minimal web presence.</li>
+						<li>Start small but keep your ability to scale up in the future.</li>
+						<li>Fit any budget; Big or Small.</li>
+					</ul>
+					<a class="button" href="/pages/contact">Get Started</a>
+				</div>
+			</section>
+			<section>
+				<h1>Need A More Custom<br>
+					 Solution?</h1>
+					 <hr>
+				<div>
+					<H2>Stand out from the rest with an end-all Web Solution for your Business. 
+						Our Custom Websites begin at just $2000 and are guaranteed to efficiently 
+						match your budget with our performance.</H2>
+					<ul>
+						<li>For larger operations with more robust requirements.</li>
+						<li>Enjoy a quick turnaround and a lightning fast response time.</li>
+						<li>Built in On-Page S.E.O. and Mobile Optimization.</li>
+					</ul>
+					<a class="button" href="/pages/contact">Get Started</a>
+				</div>
+			</section>
+	</section> -->
+
+
+	<section class="flex center" id="designSolutions">
+		<section>
+			<h1>Alenthea Does SEO Right!</h1>
+			<p>Get in front of your audience and generate natural organic traffic by improving your website's content as well as your code's internal structure.</p>
+		</section>
+	<div class="flex space-between">
+		<section>
+			<h1>Technical SEO</h1>
+			<p>Technical SEO is the foundational principal of any truly effective Search Engine Optimization strategy. 
+			   It covers your website's PageSpeed, internal structure, behavior, security, and any other technical 
+			   requirements that today's search engines look for.
+			</p>
+			<p>This is our #1 approach to differentiating our clients' websites from the competition for higher 
+			   rankings and long term domain authority.
+			</p>
+		</section>
+		<section>
+			<h1>Creative Website Design</h1>
+			<p>Local SEO allows your website to capture search traffic for the specific area's your business 
+			   operates in. We start by taking ownership and optimizing your business' profile on Google My Business,
+			   Yelp, and many others. 
+			</p>
+			<p>Then we collect and add relevant location-specific copy to your webpage content, including phone numbers, 
+			   addresses, schema markup, and local content about the specific service area's you wish to target.
+			</p>
+		</section>
+	</div>
+	</section>
+	<section id="reDesign">
+		<h1>Start with an SEO audit of your website for <br> just $199.*</h1>
+		<p>Discover what is working for your business, what could be hurting you, 
+		   and where we believe your current SEO could see some improvements.</p>
+		<a class="button" href="/pages/contact">Request An SEO Audit</a>
+	</section>	
+    <section>
+	    <div class="flex column bigOlText secondBigText">
+		    <h1>Latest Technologies;</h1>
+		    <h1>Highest Standards.</h1>
+	    </div>
+        <p class="littleOlText">Our websites work exceptionally well to deliver an intuitive and encapsulating experience for both your audience and yourself alike.</p>
+    </section>
+	<!-- <section class="full column flex center">
+		<h1 class="flex column center sectionHeader">Loaded with features:</h1>
+		<div class="flex row wrap center" id="serviceTiles">
+			<section>
+				<h1>Mobile Ready</h1>
+				<p>All of our websites are built responsive and with all devices in mind.</p>
+			</section>
+			<section>
+				<h1>Dynamic Content</h1>
+				<p>Dynamic websites are faster, more secure, and deliver fresh content.</p>
+			</section>
+			<section>
+				<h1>Domain Email</h1>
+				<p>Impress your clients with your own custom business email address.</p>
+			</section>
+			<section>
+				<h1>Intelligent S.E.O.</h1>
+				<p>Place your content in front of the right people when they need you most.</p>
+			</section>
+		</div>
+	</section> -->
+	<!-- <section class="flex center column" id="hosting">
+	<div class="flex row center">
+		<img src="/images/server.jpg" alt="Hosting Illustration">
+		
+		<div class="flex center column">
+			<h1>HOSTING</h1>
+			<h2>Low to No Cost</h2>
+		</div>
+		</div>
+		<p class="littleOlText secondLittleText">Find the perfect hosting solution for your Website. With scale-able options, you can pay for exactly what you need while allowing room to grow. Let us see how much you could be saving.</p>
+			<a class="button disabled">Click Here For Hosting Solutions</a>
+	</section> -->
+	<section id="ecommerce" class="flex column bigOlText secondBigText">
+		<div class="flex column">
+			<h1>E-Commerce</h1>
+			<p>Take your business online and market your products across the most popular platforms and maximize your profits through exposure to a to a global market.</p>
+			<a class="button" href="/services/ecommerce">Click Here For Better Conversions</a>
+		</div>
+	</section>
+	<?php include "../sections/contact.php";?>
+	<br><br>
+	<?php include "../sections/footer.php";?>
+</body>
+</html>
